007fe87217e477f5c0ee1afbe4be4d1b0892a496
badar5uk <bader5uk@hotmail.com>
2024-09-25 12:15:52 +0400

    Add files via upload

---
+++ copiesOfFirstChar.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class copiesOfFirstChar {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a word");
+        String userInp = scan.next();
+        if (userInp.substring(0).equals(userInp.substring(userInp.length() - 1))) {
+            System.out.print(userInp.substring(0));
+            System.out.print(userInp.substring(0));
+            System.out.print(userInp.substring(0));
+        } else if ((userInp.substring(0, 2).equals(userInp.substring(userInp.length() - 2)))) {
+            System.out.print(userInp.substring(0, 2));
+            System.out.print(userInp.substring(0, 2));
+            System.out.print(userInp.substring(0, 2));
+        } else if ((userInp.substring(0, 2).contains(userInp.substring(userInp.length())))) {
+            System.out.print(userInp.substring(0, 2));
+            System.out.print(userInp.substring(0, 2));
+            System.out.print(userInp.substring(0, 2));
+        }
+    }
+}
